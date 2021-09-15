88a283dd73ec7dd730510610efa3a7b6c76e8b50
Tomas Bylund <tomas.bylund@lnu.se>
2021-09-15 14:17:35 +0000

    Updated to unclude saving the project config, and creating a root config file from hardcoaded values

--- scripts/setup_proj_directory.py
+++ scripts/setup_proj_directory.py
@@ -2,6 +2,8 @@ import argparse as arg
 import configparser as con
 import os
 import pathlib as p
+import shutil as sh
+import datetime as dt
 
 setup_proj_args = arg.ArgumentParser()
 setup_proj_args.add_argument("--config", type=str, required=True,
@@ -17,6 +19,7 @@ parser.read(args.config,encoding="utf-8")
 
 data = p.Path(parser["paths"]["data_dir"])
 logs = p.Path(parser["paths"]["logs_dir"])
+proj = p.Path(parser["paths"]["project_dir"])
 
 if parser.has_option("paths","storage_loc"):
     store = p.Path(parser["paths"]["storage_loc"])
@@ -25,11 +28,11 @@ if parser.has_option("paths","storage_loc"):
     if not os.path.isdir(data_store):
         print(f"Creating {data_store}")
         os.mkdir(data_store)
+        data.symlink_to(data_store)
     if not os.path.isdir(logs_store):
         print(f"Creating {logs_store}")
         os.mkdir(logs_store)
-    data.symlink_to(data_store)
-    logs.symlink_to(logs_store)
+        logs.symlink_to(logs_store)
 else:
     if os.path.isdir(data):
         print(f"Creating {data}")
@@ -38,3 +41,26 @@ else:
         print(f"Creating {logs}")
         os.mkdir(logs)
 
+if not os.path.isdir(proj / "config"):
+    config_dir = proj / "config"
+    config_file = os.path.basename(args.config)
+    print(f"Creating {config_dir} and copying to it")
+    os.mkdir(config_dir)
+    sh.copy(args.config,config_dir)
+    os.rename(config_dir / config_file,
+              config_dir / f"PROJCONF_{dt.datetime.now().strftime('%Y-%m-%d+%H:%M:%S')}_{config_file}")
+
+proj_config = proj / "config/root_config.ini"
+proj_config_content = """[DEFAULT]
+save-model-dir =
+gpus = 1
+seed = 352351
+log-interval = 500
+keep-top-N-checkpoints = 5,
+terminate_on_nan = True
+max-epochs = 20
+batch-size = 16
+loader-workers = 4
+"""
+with open(proj_config,"w") as fil:
+    fil.writelines(proj_config_content)
